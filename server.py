5db7db9b3d6e54d84c15da042606ba236aafc9d5
piotrek <piotrek@ubuntu.com>
2025-02-24 08:39:08 +0100

    init commit

---
+++ server.py
@@ -0,0 +1,24 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# TODO zrobi strone flask
+# TODO zrobic zmienne srodowiskowe
+# TODO zrobic zrobic tabele i poloczanie z baza danych
+# TODO zrobic strone glowna ktora wyswietla wszystkie rzeczy z bazy danych
+# todo zrobic formularz do dodawania kawiarni
+# todo zrobic
+# todo zrobic funkcjonalosc ktora dodaje punkty
+# TODO zrobic api tak zeby kazdy mogl modyfikowac ta liste i dokumentacje do tego
+
